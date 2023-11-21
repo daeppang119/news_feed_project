add12f9faf72bc64647e27fc836e6e84c4d393cd
최문길 <cmk0905@naver.com>
2023-11-21 21:47:14 +0900

    불필요한 것들 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
 import React from "react";
-import GlobalStyle from "./StyledComponents/GlobalStyle";
-import Router from "./shared/Router";
 import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./StyledComponents/GlobalStyle";
 import theme from "./StyledComponents/theme/theme";
-import Sample from "./components/Sample/Sample";
 import { fireBase } from "./firebase/firebase";
-/*
-- router 설치 완료
-- styled component 설치 완료
-- redux 설치 완료 하였습니다.
-- firebase 설치 완료
-
-
-Pages에는 회원가입, 메인화면, 개인페이지  총 3개로 나누었습니다. 
-
-StyledComponents의 modules를 가보시면 아시겠지만 거기에 text 있숨다.
-components 폴더의 Sample에 가보시면 거기에 text 있습니다.
-redux 폴더의 module을 가보시면 text가 있습니다. 
-
-StyledComponents/modules/StyledTest를 읽고 난 후 삭제 해주시면 감사하겠습니다.
-Components/Sample을 이해하고 난 후 삭제 해 주시면 감사하겠습니다.
-redux/ moduls/ 이거봐주세요 text를 읽고 난 후 삭제해 주세요.
-*/
+import Router from "./shared/Router";
 
 function App() {
   console.log(fireBase);
@@ -29,8 +11,6 @@ function App() {
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        {/* Sample Component는 테스트용입니다. Sample을 보신 후 반드시 삭제해 주셔요 */}
-        <Sample />
         <Router />
       </ThemeProvider>
     </>
