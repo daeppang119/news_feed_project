d717ac7e0ecee5b49b531889c862ea60305d4373
최문길 <cmk0905@naver.com>
2023-11-25 03:06:24 +0900

    강나연님과 함께 작업한 카테고리 변화시 페이지 처리

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,22 +1,21 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import AddForm from "../components/AddForm";
-import Category from "../components/Category/Category";
 import Header from "../components/Header";
 import MainPage from "../components/MainPage/MainPage";
 
 function Main() {
   const post = useSelector((state) => state.post);
-  const [categorizedPosts, setCategorizedPosts] = useState(post);
+
   const [isOpen, setIsopen] = useState(false);
   const [title, setTitle] = useState("");
   const [contents, setContents] = useState("");
-
+  // 카테고리 클릭 or onChange 하면 post에서 filtering 처리하여 mainPage뜯어서 보여주세요, 요점은 post 원본은 냅둬야 하므로 state를 사용 하여 처리 하자!
   return (
     <>
       <Header setIsopen={setIsopen} setTitle={setTitle} setContents={setContents} />
-      <Category setCategorizedPosts={setCategorizedPosts} />
-      <MainPage categorizedPosts={categorizedPosts} />
+
+      <MainPage />
       <AddForm
         isOpen={isOpen}
         setIsopen={setIsopen}
--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import * as St from "../../StyledComponents/modules/StyledCategory/StyledCategory";
 
-function Category({ setCategorizedPosts }) {
+function Category({ setCategorizedBox }) {
   const post = useSelector((state) => state.post);
   const [isLoged, setIsLoged] = useState(false);
   const [categoryCount, setCategoryCount] = useState(0);
@@ -10,66 +10,66 @@ function Category({ setCategorizedPosts }) {
 
   const onChangeHandler = (event) => {
     const categorizedPost = post.filter((item) => item.category === event.target.value);
-
-    categorizedPost.length == 0 ? setCategorizedPosts(post) : setCategorizedPosts(categorizedPost);
-    setCategoryCount(categorizedPost.length);
+    setCategorizedBox(categorizedPost);
   };
-  console.log(categoryCount);
 
   const onClickHandler = (event) => {
     const categorizedPost = post.filter((item) => item.category === event.target.textContent);
-
-    categorizedPost.length == 0 ? setCategorizedPosts(post) : setCategorizedPosts(categorizedPost);
+    setCategorizedBox(categorizedPost);
   };
   return (
-    <St.Container>
-      <St.UserInfo>
-        {!isLoged ? (
-          <>
-            <St.LoginBtn onClick={() => setIsLoged(true)}>로그인</St.LoginBtn>
-            <St.LoginMessage>
-              최애의 아이들이 되어
-              <br />
-              일상을 공유해보세요
-            </St.LoginMessage>
-          </>
-        ) : (
-          <>
-            <St.UserProfile>
-              <img src={process.env.PUBLIC_URL + "/categoryimg/usericon.png"} />
-              <St.UserNameEmail>
-                <St.UserName>최애의 아이들</St.UserName>
-                <St.UserEmail>testB2B2B2@gmail.com</St.UserEmail>
-              </St.UserNameEmail>
-            </St.UserProfile>
-            <St.PostLike>
-              <St.Post>
-                <p>게시글</p>
-                <p>9개</p>
-              </St.Post>
-              <St.Like>
-                <p>Today♥︎</p>
-                <p>200</p>
-              </St.Like>
-            </St.PostLike>
-            <St.Logout onClick={() => setIsLoged(false)}>Logout</St.Logout>
-          </>
-        )}
-      </St.UserInfo>
+    <>
+      <St.Container>
+        <St.UserInfo>
+          {!isLoged ? (
+            <>
+              <St.LoginBtn onClick={() => setIsLoged(true)}>로그인</St.LoginBtn>
+              <St.LoginMessage>
+                최애의 아이들이 되어
+                <br />
+                일상을 공유해보세요
+              </St.LoginMessage>
+            </>
+          ) : (
+            <>
+              <St.UserProfile>
+                <img src={process.env.PUBLIC_URL + "/categoryimg/usericon.png"} alt="" />
+                <St.UserNameEmail>
+                  <St.UserName>최애의 아이들</St.UserName>
+                  <St.UserEmail>testB2B2B2@gmail.com</St.UserEmail>
+                </St.UserNameEmail>
+              </St.UserProfile>
+              <St.PostLike>
+                <St.Post>
+                  <p>게시글</p>
+                  <p>9개</p>
+                </St.Post>
+                <St.Like>
+                  <p>Today♥︎</p>
+                  <p>200</p>
+                </St.Like>
+              </St.PostLike>
+              <St.Logout onClick={() => setIsLoged(false)}>Logout</St.Logout>
+            </>
+          )}
+        </St.UserInfo>
 
-      <St.CategorySelect>
-        <select onChange={onChangeHandler}>
+        <St.CategorySelect>
+          <select onChange={onChangeHandler}>
+            {categoryArr.map((item) => (
+              <option key={item}>{item}</option>
+            ))}
+          </select>
+        </St.CategorySelect>
+        <St.CategoryBar>
           {categoryArr.map((item) => (
-            <option key={item}>{item}</option>
+            <p key={item.id} onClick={onClickHandler}>
+              {item}
+            </p>
           ))}
-        </select>
-      </St.CategorySelect>
-      <St.CategoryBar>
-        {categoryArr.map((item) => (
-          <p onClick={onClickHandler}>{item}</p>
-        ))}
-      </St.CategoryBar>
-    </St.Container>
+        </St.CategoryBar>
+      </St.Container>
+    </>
   );
 }
 
